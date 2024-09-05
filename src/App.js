23651ae0b403e8f4d1c4fda73746ec5a1314d175
Bink <tuanbinhnguyenngoc@gmail.com>
2024-09-05 17:21:21 +0700

    re-css

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ function App() {
   const [accounts, setAccounts] = useState([]);
 
   return (
-    <div className="overlay">
-      <div className="App relative h-full">
+    <div className="overlay opacity-85 w-full h-full z-10 fixed top-5 left-5">
+      <div className="App relative h-full text-center text-white">
         <Navbar accounts={accounts} setAccounts={setAccounts} />
         <MainMint accounts={accounts} setAccounts={setAccounts}></MainMint>
         <Footer />
       </div>
-      <div className="moving-background"></div>
+      <div className="-z-10 absolute top-0 left-0 right-0 bottom-0 bg-[url('./assets/bg/city2.webp')] bg-no-repeat bg-cover bg-fixed"></div>
     </div>
   );
 }
